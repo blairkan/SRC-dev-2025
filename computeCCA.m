c1a4308f060966c8bace3b1aa3138de288d4be4a
Blair Kaneshiro <blairbo@gmail.com>
2026-02-03 17:15:19 -0900

    replace A, B outputs with H, W

--- computeCCA.m
+++ computeCCA.m
@@ -1,5 +1,5 @@
-function [A,B,rhos,pvals,U,V,Rxx,Ryy] = computeCCA(X,Y,Kx,Ky)
-%[A,B,rhos,pvals,U,V] = myCanonCorr(X,Y,Kx,Ky) 
+function [H,W,rhos,pvals,U,V,Rxx,Ryy] = computeCCA(X,Y,Kx,Ky)
+%[H,W,rhos,pvals,U,V] = myCanonCorr(X,Y,Kx,Ky) 
 % -----------------------------------------------------------
 % Blair dev version 2026
 % Regularized canonical correlation
@@ -29,15 +29,15 @@ function [A,B,rhos,pvals,U,V,Rxx,Ryy] = computeCCA(X,Y,Kx,Ky)
 % - 2/3/2026: Blair - adapted from myCanonCorr
 
 if nargin<2, error('JD: at least two argument required'); end
-if ~ismatrix(X), error('JD: X must have two dimensions'); end;
-if ~ismatrix(Y), error('JD: Y must have two dimensions'); end;
-if size(X,1)>size(X,2), X=X.'; warning('JD: transposing X'); end;
-if size(Y,1)>size(Y,2), Y=Y.'; warning('JD: transposing Y'); end;
+if ~ismatrix(X), error('JD: X must have two dimensions'); end
+if ~ismatrix(Y), error('JD: Y must have two dimensions'); end
+if size(X,1)>size(X,2), X=X.'; warning('JD: transposing X'); end
+if size(Y,1)>size(Y,2), Y=Y.'; warning('JD: transposing Y'); end
 
 
 [Rxy,Rxx,Ryy,Ryx] = nanRXY(X,Y);
-if nargin<4 || isempty(Ky), Ky=rank(Ryy); end;
-if nargin<3 || isempty(Kx), Kx=rank(Rxx); end;
+if nargin<4 || isempty(Ky), Ky=rank(Ryy); end
+if nargin<3 || isempty(Kx), Kx=rank(Rxx); end
 
 % compute A
 Rxxnsq=regSqrtInv(Rxx,Kx); % regularized Rxx^(-1/2)
@@ -46,24 +46,24 @@ M = (M+M')/2; % fix nummerical precision asymmetric
 [C,Dc]=eig(M);  % textbook
 [~,indx]=sort(diag(Dc),'descend'); 
 C=C(:,indx(1:min(Kx,Ky))); % dump zero eigenvalue dimensions 
-A=Rxxnsq*C; % invert coordinate transformation
+H=Rxxnsq*C; % invert coordinate transformation
 
 % compute B
 Ryynsq=regSqrtInv(Ryy,Ky); % regularized Ryy^(-1/2)
 D=Ryynsq*Ryx*Rxxnsq*C;
-B=Ryynsq*D;
+W=Ryynsq*D;
 
 
 %[A,Da]=eig(regInv(Rxx,Kx)*Rxy*regInv(Ryy,Ky)*Ryx);
 %[B,Db]=eig(regInv(Ryy,Ky)*Ryx*regInv(Rxx,Kx)*Rxy);
 
-if sum(~isreal(A(:))) | sum(~isreal(B(:)))
+if sum(~isreal(H(:))) | sum(~isreal(W(:)))
     warning('Imaginary components. Something is wrong!'); 
-    A=real(A); B=real(B);
+    H=real(H); W=real(W);
 end
 
-U=A.'*X;
-V=B.'*Y;
+U=H.'*X;
+V=W.'*Y;
 
 nVars=min(size(U,1),size(V,1));
 rhos=zeros(nVars,1);
@@ -79,7 +79,7 @@ return
 
 X=randn(5,1000);
 Y=randn(5,1000);
-[A,B,rhos,pvals,U,V] = myCanonCorr(X,Y,5,5);
+[H,W,rhos,pvals,U,V] = myCanonCorr(X,Y,5,5);
 rhos
 pvals
 
