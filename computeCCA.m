1f2bdd24ee34f3ad599779dabd64d965eb2fa9e8
Blair Kaneshiro <blairbo@gmail.com>
2026-02-03 17:42:06 -0900

    update fn and stop after filter calculations

--- computeCCA.m
+++ computeCCA.m
@@ -1,54 +1,66 @@
-function [H,W,rhos,pvals,U,V,Rxx,Ryy] = computeCCA(X,Y,Kx,Ky)
-%[H,W,rhos,pvals,U,V] = myCanonCorr(X,Y,Kx,Ky) 
+function [H, W, U, V, A, dC, R] = computeCCA(X, Y, Kx, Ky)
+% [H, W, U, V, A, dC, R] = computeCCA(X, Y, Kx, Ky)
 % -----------------------------------------------------------
 % Blair dev version 2026
 % Regularized canonical correlation
 % 
-% Inputs
+% Inputs (required)
 % - X: Stimulus feature convolution matrix: [time x lags] with intercept
 %   column of 1s at the end
 % - Y: EEG data: [time x electrode]
-% - Kx: Regularization parameter for the stimulus
-% - Ky: Regularization parameter for the EEG
+%
+% Inputs (optional)
+% - Kx: Regularization parameter for the stimulus. If empty or not
+%   specified, will default to the rank of covariance matrix Rxx.
+% - Ky: Regularization parameter for the EEG. If etmpty or not specified,
+%   will default to the rank of the covariance matrix Ryy. 
 %
 % Outputs
 % - H: Set of temporal filters [lag x component] 
 %   --> Used to be "A"
 % - W: Set of spatial filters [electrode x component] 
 %   --> Used to be "B"
-% - A: Forward model of W for topoplots 
-%   --> New
 % - U: Temporally filtered stimulus feature [time x component] 
 %   --> transpose of previous output
 % - V: Spatially filtered EEG [time x component] 
 %   --> transpose of previous output
+% - A: Forward model of W for topoplots 
+%   --> New
+% - dC: Eigenvalues of the eigenvalue decomposition
+%   --> New
 % - R: Struct of covariance matrices Rxx, Ryy, Rxy, Ryx 
 %   --> struct instead of returning only two of them as separate variables
 
 % History
 % - 2/3/2026: Blair - adapted from myCanonCorr
 
+%% Check inputs
 if nargin<2, error('JD: at least two argument required'); end
 if ~ismatrix(X), error('JD: X must have two dimensions'); end
 if ~ismatrix(Y), error('JD: Y must have two dimensions'); end
 if size(X,1)>size(X,2), X=X.'; warning('JD: transposing X'); end
 if size(Y,1)>size(Y,2), Y=Y.'; warning('JD: transposing Y'); end
 
-
+%% Compute covariance matrices
 [Rxy,Rxx,Ryy,Ryx] = nanRXY(X,Y);
-if nargin<4 || isempty(Ky), Ky=rank(Ryy); end
-if nargin<3 || isempty(Kx), Kx=rank(Rxx); end
 
-% compute A
+% Store covariance matrices in output struct
+R.Rxy = Rxy; R.Rxx = Rxx; R.Ryy = Ryy; R.Ryx = Ryx;
+
+% Provide default Kx, Ky values if needed
+if nargin<3 || isempty(Kx), Kx=rank(Rxx); end
+if nargin<4 || isempty(Ky), Ky=rank(Ryy); end
+
+%% Compute temporal filters H 
 Rxxnsq=regSqrtInv(Rxx,Kx); % regularized Rxx^(-1/2)
 M = Rxxnsq*Rxy*regInv(Ryy,Ky)*Ryx*Rxxnsq; % textbook
 M = (M+M')/2; % fix nummerical precision asymmetric
 [C,Dc]=eig(M);  % textbook
-[~,indx]=sort(diag(Dc),'descend'); 
+[dC,indx]=sort(diag(Dc),'descend'); % NEW - save also first output as dC
 C=C(:,indx(1:min(Kx,Ky))); % dump zero eigenvalue dimensions 
 H=Rxxnsq*C; % invert coordinate transformation
 
-% compute B
+%% Compute spatial filters W
 Ryynsq=regSqrtInv(Ryy,Ky); % regularized Ryy^(-1/2)
 D=Ryynsq*Ryx*Rxxnsq*C;
 W=Ryynsq*D;
@@ -62,8 +74,20 @@ if sum(~isreal(H(:))) | sum(~isreal(W(:)))
     H=real(H); W=real(W);
 end
 
-U=H.'*X;
-V=W.'*Y;
+%% Compute spatial filter forward models A 
+A = Ryy * W * inv(W' * Ryy * W);
+
+%% Project input data through respective filters
+
+%%% Blair note: Figure out later why the dimensions are like this:
+% - H: Lag x component
+% - X: lag x time
+% - W: Channel x component
+% - Y: Channel x time
+U = X.' * H;
+V = Y.' * W;
+
+return
 
 nVars=min(size(U,1),size(V,1));
 rhos=zeros(nVars,1);
