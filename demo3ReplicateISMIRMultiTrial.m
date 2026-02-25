44fb033d139e5eb13dc978d76573e24e5da49e22
Blair Kaneshiro <blairbo@gmail.com>
2026-02-25 13:58:22 -0900

    add comprehensive documentation; update params to match ismir paper

--- demo3ReplicateISMIRMultiTrial.m
+++ demo3ReplicateISMIRMultiTrial.m
@@ -1,10 +1,14 @@
 % demo3ReplicateISMIRMultiTrial.m
 % --------------------------------
 % Blair dev version 2/18/2026
-% stim2eeg demo
+% Last updated 2/25/2026
 %
-% perform a SRC analysis on sample data from a single participant 
-% viewing a ~5.5 minute clip from the film "Dog Day Afternoon"
+% This demo shows how to compute CCA on multi-trial data. We use 24
+% responses to a full-length song. The data represent Song 1 as analyzed in
+%   Gang et al. (2017). Decoding Neurally Relevant Musical Features Using 
+%   Canonical Correlation Analysis. ISMIR. 
+%
+% This script uses computeCCA() to do the core CCA calculation.
 %
 % Workflow
 % 1. Load input data
@@ -13,7 +17,6 @@
 % 4. Call core function computeCCA.m
 % 5. Compute SRC --> moved outside of core function
 % 6. Visualize outputs
-
 
 % History
 % 2/18/2026 - adapted from demo2ComputeCCA
@@ -24,16 +27,21 @@ clear all; close all; clc
 %% Download ISMIR data
 
 % The following script will download any ISMIR files into the Data/ 
-% directory if they are not already there. For any file that is already 
-% there, the script will skip downloading a second time. The downloaded
-% files are ignored by git. 
+% directory of this repo if they are not already there. For any file that 
+% is already there, the script will skip downloading a second time. The 
+% downloaded files are ignored by git as specified in .gitignore. 
+
+% Downloaded files: 
+% 1. song21_all_Imputed.mat -- EEG data. Size: 774 MB
+% 2. 21_Features_80Hz.mat -- many individual stim features. Size: 6 MB
+% 3. 21_PC1_80Hz.mat -- principal component one of the combined individual
+%    stim features. Size: 166 KB
 
 downloadISMIRData
 
 %% Load input data
 
 %%% Load stim feature files
-
 % Gang 2017 paper: 'We performed all subsequent analyses using PC1, as 
 % well as two individual features. RMS and spectral flux were chosen as
 % they reflect amplitude envelope and timbre, respectively, and have been 
@@ -44,23 +52,23 @@ downloadISMIRData
 % feature is a column vector
 tempX1 = load('21_Features_80Hz.mat', 'fs', 'vars'); 
 
-% In this file, 'PC' is a column vector feature
+% In this file, 'PC' is a column vector single feature
 tempX2 = load('21_PC1_80Hz.mat', 'fs', 'PC');
 
 %%% Load EEG file
-
 % In this file, 'data21' is a [space x time x trial] matrix
 tempY = load('song21_all_Imputed.mat', 'fs', 'data21'); 
 
 %%% Check and extract sampling rates
-assert(isequal(tempX1.fs, tempX2.fs), 'Stim feature sampling rates are not equal!')
+assert(isequal(tempX1.fs, tempX2.fs),... 
+    'Stim feature sampling rates are not equal!')
 fsStim = tempX1.fs; % 80
 fsEEG = tempY.fs; % 125
 
-%%% Extract stim features of interest
-stimRMS = tempX1.vars.RmsEnergy;
-stimFlux = tempX1.vars.Flux; 
-stimPC1 = tempX2.PC; 
+%%% Extract stim features of interest and make sure they are columns
+stimRMS = tempX1.vars.RmsEnergy; stimRMS = stimRMS(:);
+stimFlux = tempX1.vars.Flux; stimFlux = stimFlux(:);
+stimPC1 = tempX2.PC; stimPC1 = stimPC1(:);
 
 %%% Extract EEG data matrix
 eeg = tempY.data21; % [space x time x trial] matrix, no NaNs
@@ -70,6 +78,10 @@ disp(['Input data has ' num2str(nSpace) ' channels, ' num2str(nTime) ...
 
 clear temp*
 
+% As of now, these are the current data variables: 
+% - stimRMS, stimFlux, stimPC1: Column vectors of individual features
+% - eeg: [space x time x trial] matrix of EEG data
+
 %% Downsample and DC correct the EEG
 
 %%% Downsample EEG to sampling rate of the stim feature
@@ -78,8 +90,8 @@ clear temp*
 %   linear methods there is no value add (i.e., additional information) 
 %   from upsampling to the higher sampling rate
 % - As is the case here, the stim feature often has the lower sampling rate
-% - Therefore, we are downsampling the EEG to the sampling rate of the stim
-%   feature
+% - Therefore, here we are downsampling the EEG to the sampling rate of the 
+%   stim feature
 
 % Iterate through the trials to resample each trial and save the outputs 
 % into a new 3D matrix. 
@@ -91,6 +103,7 @@ clear temp*
 for i = 1:nTrial
     eegDown(:, :, i) = resample(eeg(:, :, i)', fsStim, fsEEG)';
 end
+% clear eeg % Clean up the workspace
 
 %%% DC Correct the EEG
 % Now we will iterate through the trials again and DC correct each trial,
@@ -101,9 +114,14 @@ end
 for i = 1:nTrial    
     eegDC(:, :, i) = dcCorrect(eegDown(:, :, i));
 end
+% clear eegDown % Clean up the workspace
 
 % Get data size again
 [nSpace, nTime, nTrial] = size(eegDC);
+
+% As of now, these are the current data variables: 
+% - stimRMS, stimFlux, stimPC1: Column vectors of individual features
+% - eegDC: [space x time x trial] matrix of downsampled, DC-corrected EEG
 
 %% z-score the stim feature and match its length to the EEG
 
@@ -111,6 +129,7 @@ end
 zStimRMS = zscore(stimRMS);
 zStimFlux = zscore(stimFlux); 
 zStimPC1 = zscore(stimPC1);
+% clear stimRMS stimFlux stimPC1 % Clean up the workspace
 
 %%% Adjust length of stim feature to match length of EEG
 % - It's common for the downsampled EEG and the stimulus to have slightly
@@ -149,6 +168,11 @@ elseif length(zStimPC1) > nTime % If feature is longer than EEG
     disp('PC1: Trimming to length of EEG')
     zStimPC1 = zStimPC1(1:nTime);
 end
+clear nZero
+
+% As of now, these are the current data variables: 
+% - zStimRMS, zStimFlux, zStimPC1: Column vectors of individual features
+% - eegDC: [space x time x trial] matrix of downsampled, DC-corrected EEG
 
 %% Create matrix of time-shifted versions of stim feature
 % Now we need to create a convolution matrix from the one-dimensional
@@ -167,7 +191,7 @@ end
 % - In the future we will update this to accommodate both forward and 
 %   backward time shifts (for instance, if the EEG anticipates stimulus
 %   feature fluctuations).
-filterLength=fsStim; 
+filterLength = 2 * fsStim; % Original demo used 1 sec; Gang paper used 2 sec
 
 %%% Now create the convolution matrix
 % - This is the matrix with all the shifted versions of the stimulus
@@ -178,77 +202,100 @@ filterLength=fsStim;
 % - Input 1 is the stimulus feature vector
 % - Input 2 is the number of delayed versions we want to add. For now we
 %   want fsStim instances total, included the non-delayed version, so we
-%   specify filterLength-1 delays.
+%   specify filterLength-1 delays. Can discuss later whether we should
+%   remove the "minus 1" part.
 % - Input 3 is whether to add the intercept column at the end
 zStimRMSMatrix = createFeatureToeplitzMatrix(zStimRMS, filterLength-1, 1);
 zStimFluxMatrix = createFeatureToeplitzMatrix(zStimFlux, filterLength-1, 1);
 zStimPC1Matrix = createFeatureToeplitzMatrix(zStimPC1, filterLength-1, 1);
 
+% As of now, these are the current data variables: 
+% - zStimRMSMatrix, zStimFluxMatrix, zStimPC1Matrix: [time x lag+intercept]
+%   stimulus feature matrices
+% - eeg: [space x time x trial] matrix of EEG data
+
 %% Specify regularization parameters
 % We are ready to call the core (CCA) function, but first we need to set
 % some regularization parameters, which tell the CCA how many dimensions we
 % should keep in both the stimulus and the EEG data
-% - Blair note: Maybe leave these regularization variables as is for now, 
-%   and we can see whether changing them makes a difference in the future 
-%   (with RCA we haven't tried to tune these much)
 % - Smaller number means stronger regularization
 
 % How strongly to regularize the stimulus
-Kx=7; % Starting value = 7
+Kx=10; % Original demo used 7; Gang paper used 10
 
 % How strongly to regularize the EEG
-Ky=7; % Starting value = 7
+Ky=10; % Original demo used 7; Gang paper used 10
 
-%% Call the core CCA function
+%% Final matrix reshapings and repetitions for input to CCA
 
-%%% "X" refers to stimulus things, and "Y" refers to response things
-
-%%% As a reminder, here are the current possible inputs
+%%% As a reminder, here are the current possible inputs up to now
 % - eegDC: Downsampled, DC-corrected [space x time x trial] EEG matrix
 % - zStimRMSMatrix: Convolution matrix of RMS feature
 % - zStimFluxMatrix: Convolution matrix of Flux feature
 % - zStimPC1Matrix: Convolution matrix of PC1 feature
 
 %%% Reshape the EEG into a single space x concat time matrix
-eegConcat = eegDC(:, :)'; % [concat time x space]
+eegConcat = eegDC(:, :); % Reshape to [space x concat time]
+eegConcat = eegConcat'; % Transpose to [concat time x space]
 
-%%% Call the core function which computes the spatial and temporal filters
+%%% Specify which feature to input and repeat it nTrials times
+% Can specify zStimRMSMatrix, zStimFluxMatrix, or zStimPC1Matrix
+featureUse = zStimFluxMatrix;
+fStr = 'Flux'; % String version for figure title
+
+% Repeat the specified feature matrix nTrials times vertically
+featureUseConcat = repmat(featureUse, nTrial, 1); % [concat time x lag]
+
+% As of now, these are the current data variables: 
+% - featureUseConcat: [concat time x lag+intercept] stim feature matrix
+% - eegConcat: [concat time x space] matrix of EEG data
+
+%% Call the core CCA function
+
+%%% General note: 
+% - "X" refers to stimulus things, and "Y" refers to response things
+
+%%% Outputs of this function
 % - H: Temporal filters
 % - W: Spatial filters
 % - U: Temporally filtered stim feature
 % - V: Spatially filtered EEG
-% - A: Fwd model of spatial filters
-% - dC: Eigenvalues, R: Struct of covariance matrices
+% - A: Forward model of spatial filters (for topoplots)
+% - dC: Eigenvalues
 % - R: Struct containing all the covariance matrices
-
-%%% Specify which feature to input and repeat it nTrials times
-featureUse = zStimPC1Matrix; 
-fStr = 'PC1';
-featureUseConcat = repmat(featureUse, nTrial, 1); % [concat time x lag]
 
 [H, W, U, V, A, dC, R] = computeCCA(featureUseConcat, eegConcat, Kx, Ky); 
 
 %% Compute SRC - multi trial
 
 %%% For multiple trials, we could technically do one big correlation on 
-% the per-component concatenated trials and stim features (the next code
-% block). But we can also do the following (this code block):
-% - Reshape the stim feature and EEG back to 3d [time x space x trial]
-%   matrices
-% - For each component, compute SRC for each trial
-% - Report the average SRC across all trials
+% the per-component concatenated trials and stim features -- essentially 
+% treating the concatenated trial data as one big trial. 
+%
+% However, this may not be prefereable if 
+% 1. we seek *distributions* of SRC (1 SRC value per trial), or 
+% 2. we wish to avoid correlating across the discontinuities between trial  
+%    boundaries. 
+%
+% Therefore, here we compute SRC on a trial-wise basis, as follows:
+% 1. Reshape the stim feature and EEG back to 3d [time x space x trial]
+%    matrices
+% 2. For each trial, compute SRC for each component and save each value in
+%    an overall [trial x component] SRC matrix
+% 3. Report the average SRC across all trials
+
+nComp = min(size(U, 2), size(V, 2)); % Number of components to work with
 
 %%% Reshape filtered stim feature matrix
 % Note that each copy (trial) of the stim feature is the same since we were
 % multiplying a repeated single input feature matrix by the same temporal
 % filter
-nComp = min(size(U, 2), size(V, 2)); % Number of components to work with
-U3d = reshape(U', [nComp, nTime, nTrial]); % [space x time x trial[
-U3d = permute(U3d, [2 1 3]); % [time x space x trial]
+U3d = reshape(U', [nComp, nTime, nTrial]); % [space x time x trial]
+U3d = permute(U3d, [2 1 3]); % [time x space x trial] for correlations
 
 %%% Reshape filtered EEG matrix
 V3d = reshape(V', [nComp, nTime, nTrial]); % [space x time x trial]
-V3d = permute(V3d, [2 1 3]); % [time x space x trial]
+V3d = permute(V3d, [2 1 3]); % [time x space x trial] for correlations
 
 %%% Now iterate through the trials AND the components
 % - Looping for clarity; can probably vectorize for speed later
@@ -272,6 +319,8 @@ end
 
 % Now take the mean across the trial dimension to get the mean SRC for each
 % component
+disp([ newline 'SRC (' fStr ...
+    ') computed separately on each trial then averaged:'])
 mean(allRho, 1)'
 
 %% Compute SRC - single trial
@@ -289,11 +338,20 @@ for i = 1:nComp
     rhos(i) = corr(U(:,i), V(:,i));
 end
 
+disp([newline 'SRC (' fStr ...
+    ') computed across all trials as single matrix:'])
 rhos
 
 %% Visualize results
 
-% TODO: Add option for consistent clim and ylim across plots
+% 2/25/2026: This is a quick implementation using the plotOnEgi() function
+% since the topoplot_new() function is having issues with EGI .sfp
+% locations files at the moment. This block should work with EGI montages
+% containing 124, 125, 128, and 129 channels with no problem. 
+% - Later: Add a row to the figure to show the temporal filter in the
+%   frequency domain
+% - Later: Standardize the clim (top row) and ylim (bottom row). For now,
+%   each plot gets its own colorbar or ylabel for reference.
 
 close all
 nPlot = 3; 
@@ -306,6 +364,7 @@ for c=1:nPlot
     plotOnEgi_multi(A(:,c), 4);
     title(['Spatial response: comp ' num2str(c)]);
     colormap(jmaColors('coolhotcortex'))
+    colorbar
     
     %%% BK: Temporal filter of current CC (of stim feature)
     subplot(2,nPlot,c+nPlot);
@@ -316,7 +375,7 @@ sgtitle(fStr)
 
 return
 
-%% Visualize results - not working right now with EGI data
+%% NOT WORKING RIGHT NOW WITH EGI DATA Visualize results
 
 % 2/24/26: I'm having trouble getting the general-purpose topoplot function
 % working with EGI sfp files right now - please use preceding block and
